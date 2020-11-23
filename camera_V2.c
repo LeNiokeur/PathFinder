c84d71c6168957027e520add63a77471f8223d0b
Lea08 <59701707+Lea08@users.noreply.github.com>
2020-11-23 10:52:44 +0100

    Camera_v2

---
+++ camera_V2.c
@@ -0,0 +1,15 @@
+//
+//  camera_V2.c
+//  Projet_Robot
+//
+//  Created by Léa DAMIENS on 23/11/2020.
+//
+
+#include "camera_V2.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    system("python camera.py"),
+    return 0;
+}
---
+++ camera_V2.h
@@ -0,0 +1,13 @@
+//
+//  camera_V2.h
+//  Projet_Robot
+//
+//  Created by Léa DAMIENS on 23/11/2020.
+//
+
+#ifndef camera_V2_h
+#define camera_V2_h
+
+#include <stdio.h>
+
+#endif /* camera_V2_h */
