f4763e45b4748c2c4db37ed4927018b032962453
Lea08 <lea.damiens12@icloud.com>
2020-12-17 19:57:46 +0100

    Modified main.c

--- main.c
+++ main.c
@@ -10,26 +10,29 @@
 // Commande pour compiler :
 //gcc -o main main.c -lwiringPi -lrt -lpthread -lm -lrt -lcrypt
 
-
 int main(int argc, char *argv[]){
     // Test d'inclusion de servoMotorControl.c
-    int angle = 90;
+    //int angle = 90;
     // Temps en ms pendant lequel le servo moteur bloque sur l'angle envoyé
-    int timeSetAngle = 500;
+    //int timeSetAngle = 500;
     //setServoMotor(&angle, &timeSetAngle);
 
     // Test d'inclusion de camera_V2.c
     //initializeCamera();
 
     // Test d'inclusion de DCMotor.c
-    //initializeDCMotors();
-    
+    initializeDCMotors();
+    int timer=1000;
+    moveForward(&timer);
     // Test d'inclusion de Ultrason.c
     initializeUS_sensor();
+
     while (1)
     {
         int dist=getDistance();
         printf("Distance: %dcm\n", dist);
-        delay(1000);
+        LED(dist);
+        delay(500);
     }
+    return 0;
 }
