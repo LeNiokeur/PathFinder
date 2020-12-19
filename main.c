50e86c06f60e0aa138f8c7ef8a37e8cb492bf54c
Lea08 <lea.damiens12@icloud.com>
2020-12-19 14:53:04 +0100

    Modified main.c

--- main.c
+++ main.c
@@ -22,17 +22,56 @@ int main(int argc, char *argv[]){
 
     // Test d'inclusion de DCMotor.c
     initializeDCMotors();
-    int timer=1000;
-    moveForward(&timer);
-    // Test d'inclusion de Ultrason.c
+    
+    // Test d' inclusion de Ultrason.c
     initializeUS_sensor();
-
     while (1)
     {
+        int LeftDistance;
+        int RightDistance;
+        int angle = 90;   
+        int Leftangle = 0;
+        int Rightangle = 180;
+        int timeSetAngle = 1000;
         int dist=getDistance();
         printf("Distance: %dcm\n", dist);
-        LED(dist);
-        delay(500);
+        delay(100);
+        if (dist<30){
+            moveBackward();
+            delay(1000);
+            //Integrer servo pour connaitre la distance à gauche et à droite
+            setServoMotor(&angle, &timeSetAngle);
+            delay(1000);
+            
+            //Valeur distance à gauche
+            setServoMotor(&Leftangle, &timeSetAngle);
+            LeftDistance = getDistance();
+            printf("\n   %d   \n", LeftDistance);
+            delay(1000);
+            
+            setServoMotor(&angle, &timeSetAngle);
+            delay(1000);
+           
+            //Valeur de distance à droite
+            setServoMotor(&Rightangle, &timeSetAngle);
+            RightDistance = getDistance();
+            printf("\n   %d   \n", RightDistance);
+            delay(3000);
+            //Comparaison des deux distance
+            
+            if (LeftDistance>RightDistance) {
+                turnLeft();
+                delay(3000);
+            }
+            else if (LeftDistance<RightDistance) {
+                turnRight();
+                delay(3000);
+            }
+        }
+        else if(dist>30){
+           moveForward();
+        }
+            
     }
     return 0;
 }
