d6c6c8f3101500e679c802020b346a6e72337d9f
Lea08 <lea.damiens12@icloud.com>
2020-12-20 15:50:20 +0100

    Modified camera.py

--- camera.py
+++ camera.py
@@ -1,11 +1,12 @@
 import picamera
 import time
+import random
 
 camera = picamera.PiCamera()
 
-camera.resolution = (640, 480)
+camera.resolution = (2592, 1944)
 camera.start_preview()
 time.sleep(2)
-camera.capture('/home/pi/Desktop/PathFinder/Pictures/obstacle.jpg')
+camera.capture(randow()'/home/pi/Desktop/PathFinder/Pictures/obstacle_time.jpg')
 
 
